04e18d12849a56582c2d9e5ad930c2e73cd59fd5
Jack Henza <jackhenza@gmail.com>
2023-10-06 13:12:00 -0400

    creates db search functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const init = () => {
-  
+  document.querySelector('form').addEventListener("submit", e => {
+    e.preventDefault()
+    const input = e.target.querySelector('input#searchByID')
+    fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data =>{
+            document.querySelector('section#movieDetails h4').innerText = data.title
+            document.querySelector('section#movieDetails p').innerText = data.summary
+        })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
